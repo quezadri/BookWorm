f2c7d107b09f59317eaec82e435d95e827f18428
Michael Sanchez <60848730+MikeySz@users.noreply.github.com>
2022-05-02 23:53:31 -0400

    Major Changes to the Bookworm PHP
    Header and Footer are now in their own separate php files to be called whenever needed. Login in is half implemented. (Connection to database is set, and sign in button changes from "sign in" to "my account". Also login function works across each current page.  The customer id is stored as a session variable to be used in various other queries.  NOTICE: Cart item has slighlty shifted up,  as of right now unsure what causes this.

---
+++ footer.php
@@ -0,0 +1,44 @@
+<?php
+#footer
+echo "    <!-- ---------------------footer------------------- -->
+    <div class='footer'>
+      <div class='container'>
+        <div class='row'>
+          <div class='footer-col-1'>
+            <h3>Download Our App</h3>
+            <p>Download App for Android and ios mobile phone.</p>
+            <div class='app-logo'>
+              <img src='images/Playstore.png' />
+              <img src='images/Applestore.png' />
+            </div>
+          </div>
+          <div class='footer-col-2'>
+            <img src='images/BookWormLogo.gif' alt='BookWorm-Logo' height='150' width='50'></img>
+            <p>
+              A Better Way to Buy Books Online!
+            </p>
+          </div>
+          <div class='footer-col-3'>
+            <h3>Useful Links</h3>
+            <ul>
+              <li>Coupons</li>
+              <li>Blog Post</li>
+              <li>Return Policy</li>
+              <li>Join Affiliate</li>
+            </ul>
+          </div>
+          <div class='footer-col-4'>
+            <h3>Follow us</h3>
+            <ul>
+              <li>Facebook</li>
+              <li>Youtube</li>
+              <li>Instagram</li>
+              <li>Twitterr</li>
+            </ul>
+          </div>
+        </div>
+        <hr />
+        <p class='copyright'>Copyright 2022 - BookWorm</p>
+      </div>
+    </div>";
+?>
